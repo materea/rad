8c69f9b1e11f6e4571fb38f54ed7819e2b76b01f
veet-co <115666266+materia-ob-gyn@users.noreply.github.com>
2023-11-26 22:42:43 +0100

    ...

--- main.js
+++ main.js
@@ -40,3 +40,52 @@ if (prefersDarkMode) {
     darkModeToggle.checked = true;
     toggleDarkMode();
 }
+
+
+ // Function to generate dynamic Table of Contents
+ function generateTableOfContents() {
+    // Select all sections with 'id' attribute starting with 'header-'
+    var sections = document.querySelectorAll('[id^="header-"]');
+    
+    // Select the ul element to append TOC items
+    var tocList = document.getElementById('tocList');
+
+    var currentList = tocList;
+
+    sections.forEach(function(section, index) {
+        // Get the header text (h2 or h3) within the section
+        var headerText = section.querySelector('header h2').textContent;
+
+        // Create a list item with a link to the section
+        var listItem = document.createElement('li');
+        var link = document.createElement('a');
+        link.href = '#' + section.id;
+        link.textContent = headerText;
+        listItem.appendChild(link);
+
+        // Check if the section has a nested section (second level)
+        var nestedSections = section.querySelectorAll('section');
+        if (nestedSections.length > 0) {
+            // Create a sublist for second-level headers
+            var sublist = document.createElement('ul');
+
+            nestedSections.forEach(function(nestedSection) {
+                var nestedHeader = nestedSection.querySelector('header h3').textContent;
+                var nestedListItem = document.createElement('li');
+                var nestedLink = document.createElement('a');
+                nestedLink.href = '#' + nestedSection.id;
+                nestedLink.textContent = nestedHeader;
+                nestedListItem.appendChild(nestedLink);
+                sublist.appendChild(nestedListItem);
+            });
+
+            listItem.appendChild(sublist);
+        }
+
+        // Append the list item to the Table of Contents
+        tocList.appendChild(listItem);
+    });
+}
+
+// Call the function to generate Table of Contents
+generateTableOfContents();
